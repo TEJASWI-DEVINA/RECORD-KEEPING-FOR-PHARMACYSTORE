4d6d0947b9fc2cad8694cf8a4ebd3025c255e782
TEJASWI-DEVINA <78832726+TEJASWI-DEVINA@users.noreply.github.com>
2021-04-15 18:29:27 +0530

    Update medical.c

--- Implementation/src/medical.c
+++ Implementation/src/medical.c
@@ -383,11 +383,11 @@ void KnowInfoAboutMedicine(int number){
   }
  }
 int StockOfMedicine(int number){
-  int i;
+  //int i;
   if(number!=0)
   {
       printf("All Available Items are\n");
-   for(i=0;i<number;i++)
+   for(int i=0;i<number;i++)
    {
     if(m[i].id!=0){
     printf("Id=%d\nName=%s\t\tPrice=%d\tAvailable Quantity=%d\nCompany=%s\t\tMfg Date=%s\tExp Date=%s\n",m[i].id,m[i].medicneName,m[i].price,m[i].quantity,m[i].Company,m[i].Mfg_Date,m[i].Exp_Date);
